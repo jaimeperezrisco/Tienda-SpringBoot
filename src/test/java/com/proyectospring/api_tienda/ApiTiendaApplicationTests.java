7441d0590489e35b44e0fda67ac8a9e731ac1435
jaimeperezrisco <jaime.perez00vsl@gmail.com>
2025-12-10 09:31:35 +0000

    Iniciando proyecto

---
+++ src/test/java/com/proyectospring/api_tienda/ApiTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectospring.api_tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
