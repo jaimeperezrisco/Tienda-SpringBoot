d608b5062625d5bbd95cf20baa67a142db40c814
jaimeperezrisco <jaime.perez00vsl@gmail.com>
2025-12-14 20:21:14 -0100

    cambio en Repository

--- src/main/java/com/proyectospring/api_tienda/Repository/EmpleadoRepository.java
+++ src/main/java/com/proyectospring/api_tienda/Repository/EmpleadoRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.proyectospring.api_tienda.Modelo.Empleado;
 
 @Repository
-public interface EmpleadoRepository extends JpaRepository <Empleado, Integer> {
+public interface EmpleadoRepository extends JpaRepository <Empleado, Long> {
 
 }
--- src/main/java/com/proyectospring/api_tienda/Repository/ProductoRepository.java
+++ src/main/java/com/proyectospring/api_tienda/Repository/ProductoRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.proyectospring.api_tienda.Modelo.Producto;
 
 @Repository
-public interface ProductoRepository extends JpaRepository <Producto, Integer> {
+public interface ProductoRepository extends JpaRepository <Producto, Long> {
 
 }
